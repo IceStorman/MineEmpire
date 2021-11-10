5084260f630e212bb0f15059fef678c498f6533e
IceStorman <vladislavmartinisin@gmail.com>
2021-11-10 17:58:24 +0200

    Changed prestige mechanics script

--- Assets/Scripts/PrestigeMechanics.cs
+++ Assets/Scripts/PrestigeMechanics.cs
@@ -6,10 +6,25 @@ public class PrestigeMechanics : MonoBehaviour
     [SerializeField] private GameObject prestigePanel;
     [SerializeField] private GameObject upgradePanel;
 
+    [SerializeField] private Text moneyText;
+
+    [SerializeField] private Text expText;
+    [SerializeField] private Text maxExpText;
+    [SerializeField] private Text lvlText;
+
+    [SerializeField] private Text phoneCostText;
+    [SerializeField] private Text phoneText;
+    [SerializeField] private Image phoneImage;
+    [SerializeField] private Image buyPhoneButtonImage;
+
+    [SerializeField] private Text pcCostText;
+    [SerializeField] private Text pcText;
+    [SerializeField] private Image pcImage;
+    [SerializeField] private Image buyPcButtonImage;
+
     [SerializeField] private Text flatText;
     [SerializeField] private Text flatCostText;
-
-    [SerializeField] private Image thingImage;
+    [SerializeField] private Image flatImage;
     [SerializeField] private Image buyFlatButtonImage;
 
     [SerializeField] private MainData mainData;
@@ -30,29 +45,34 @@ public class PrestigeMechanics : MonoBehaviour
         prestigePanel.SetActive(false);
     }
 
-    private void UpgradeThing(OtherData otherData, PrestigeData prestigeData)
+    private void UpgradeThing(PrestigeData prestigeData, Text thingCostText)
     {
-        if(prestigeData.canUpgrade && otherData.money >= prestigeData.thingCost)
+        if(prestigeData.canUpgrade && mainData.otherData.money >= prestigeData.thingCost)
         {
-            otherData.money -= prestigeData.thingCost;
+            mainData.otherData.money -= prestigeData.thingCost;
+            moneyText.text = mainData.otherData.money.ToString("F2");
 
             if (!prestigeData.wasBought)
             {
-                otherData.lastIndex++;
+                mainData.otherData.lastIndex++;
                 prestigeData.wasBought = true;
             }
 
-            otherData.exp += prestigeData.giveExp;
+            mainData.otherData.exp += prestigeData.giveExp;
+            expText.text = mainData.otherData.exp.ToString();
+
             prestigeData.thingCost *= 0.3f;
+            thingCostText.text = prestigeData.thingCost.ToString("F2");
         }
     }
 
-    private void CanBuyLogic(OtherData otherData, PrestigeData prestigeData, 
+    private void CanBuyLogic(PrestigeData prestigeData, 
         Text thingText, Text thingCostText, Image thingImage, Image buyButtonImage)
     {
-        if(prestigeData.thingID == otherData.lastIndex + 1)
+        if(prestigeData.thingID <= mainData.otherData.lastIndex + 1)
         {
             buyButtonImage.sprite = prestigeData.unlockButtonSprite;
+            thingImage.sprite = prestigeData.unlockThingSprite;
             thingText.text = prestigeData.thingText;
             thingCostText.text = prestigeData.thingCost.ToString("F2");
             prestigeData.canUpgrade = true;
@@ -61,17 +81,20 @@ public class PrestigeMechanics : MonoBehaviour
 
     public void UpgradePhone()
     {
-
+        CanBuyLogic(mainData.phoneData, phoneText, phoneCostText, phoneImage, buyPhoneButtonImage);
+        UpgradeThing(mainData.phoneData, phoneCostText);
     }
 
     public void UpgradePC()
     {
-
+        CanBuyLogic(mainData.PCData, pcText, pcCostText, pcImage, buyPcButtonImage);
+        UpgradeThing(mainData.PCData, pcCostText);
     }
 
     public void UpgradeFlat()
     {
-
+        CanBuyLogic(mainData.flatData, flatText, flatCostText, flatImage, buyFlatButtonImage);
+        UpgradeThing(mainData.flatData, flatCostText);
     }
 
     public void OpenUpgrdePanel()
