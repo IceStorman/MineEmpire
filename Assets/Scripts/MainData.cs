282d10da132835d35a4c16c78697d9991b0e994f
IceStorman <vladislavmartinisin@gmail.com>
2021-11-19 12:24:13 +0200

    Added prestige things

--- Assets/Scripts/MainData.cs
+++ Assets/Scripts/MainData.cs
@@ -9,11 +9,21 @@ public class MainData : ScriptableObject
     public OreData benitoiteData;
     public OreData rubyData;
     public OreData emeraldData;
+
     public OtherData otherData;
+
     public MineData classicMineData;
     public MineData ironMineData;
     public MineData goldMineData;
+
     public PrestigeData phoneData;
     public PrestigeData PCData;
     public PrestigeData flatData;
+    public PrestigeData carData;
+    public PrestigeData mansionData;
+    public PrestigeData helicopterData;
+    public PrestigeData yachtData;
+    public PrestigeData privatePlaneData;
+    public PrestigeData rocketData;
+    public PrestigeData planetData;
 }
--- Assets/Scripts/SpritesData.cs
+++ Assets/Scripts/SpritesData.cs
@@ -6,4 +6,11 @@ public class SpritesData : ScriptableObject
     public PrestigeSpritesData phoneSprites;
     public PrestigeSpritesData pcSprites;
     public PrestigeSpritesData flatSprites;
+    public PrestigeSpritesData carData;
+    public PrestigeSpritesData mansionData;
+    public PrestigeSpritesData helicopterData;
+    public PrestigeSpritesData yachtData;
+    public PrestigeSpritesData privatePlaneData;
+    public PrestigeSpritesData rocketData;
+    public PrestigeSpritesData planetData;
 }
