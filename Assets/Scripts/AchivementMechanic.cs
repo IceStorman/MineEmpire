89e282b803ffdd7d50904e326aacb1db8b4cf6dc
Jok55r <81425160+Jok55r@users.noreply.github.com>
2021-12-12 19:34:46 +0100

    Starting Added achivements

---
+++ Assets/Scripts/AchivementMechanic.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchivementMechanic : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    private bool isOpened = false;
+    [SerializeField] private Button buttonPrefab;
+    [SerializeField] private Text textPrefab;
+    public List<int> moneysList = new List<int>();
+    [SerializeField] Image checkerOn;
+
+    void Start()
+    {
+        SpawnPefabs();
+        panel.SetActive(false);
+    }
+
+    void SpawnPefabs()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            Vector2 spawnPointButton = new Vector2(-493, 426 - (i * 12.5f));
+            Button buttonClone;
+            buttonClone = Instantiate(buttonPrefab, spawnPointButton, Quaternion.identity);
+            buttonClone.transform.parent = GameObject.Find("AchivementPanel").transform;
+
+            textPrefab.text = $"Earn {moneysList[i]} Money";
+            Vector2 spawnPointText = new Vector2(-405, 420 - (i * 12.5f));
+            Text textClone;
+            textClone = Instantiate(textPrefab, spawnPointText, Quaternion.identity);
+            textClone.transform.parent = GameObject.Find("AchivementPanel").transform;
+        }
+    }
+
+    public void Click()
+    {
+        buttonPrefab.image = checkerOn;
+    }
+
+    public void OpenClose()
+    {
+        if (!isOpened)
+        {
+            panel.SetActive(true);
+            isOpened = !isOpened;
+        }
+        else if (isOpened)
+        {
+            panel.SetActive(false);
+            isOpened = !isOpened;
+        }
+    }
+}
