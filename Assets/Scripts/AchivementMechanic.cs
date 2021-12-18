9f9c96e45d4b4896307aa3ebcdb1f6dbd4a69157
Jok55r <81425160+Jok55r@users.noreply.github.com>
2021-12-18 14:11:17 +0100

    completed achivements

--- Assets/Scripts/AchivementMechanic.cs
+++ Assets/Scripts/AchivementMechanic.cs
@@ -9,18 +9,18 @@ public class AchivementMechanic : MonoBehaviour
     [SerializeField] private Button buttonPrefab;
     [SerializeField] private Text textPrefab;
     public List<int> moneysList = new List<int>(9);
-    [SerializeField] Image checkerOn;
     [SerializeField] MainData mainData;
+    [SerializeField] private Sprite checkerOn;
 
-    public Image image1;
-    public Image image2;
-    public Image image3;
-    public Image image4;
-    public Image image5;
-    public Image image6;
-    public Image image7;
-    public Image image8;
-    public Image image9;
+    [SerializeField] private Image image1;
+    [SerializeField] private Image image2;
+    [SerializeField] private Image image3;
+    [SerializeField] private Image image4;
+    [SerializeField] private Image image5;
+    [SerializeField] private Image image6;
+    [SerializeField] private Image image7;
+    [SerializeField] private Image image8;
+    [SerializeField] private Image image9;
 
     private void Start()
     {
@@ -29,33 +29,26 @@ public class AchivementMechanic : MonoBehaviour
 
     private void Update()
     {
-        /*Image[] imagesMass =
-        {
-            image1,
-            image2,
-            image3,
-            image4,
-            image5,
-            image6,
-            image7,
-            image8,
-            image9,
-        };*/
+        if (mainData.otherData.money >= moneysList[0] || mainData.achivementData.moneysAchivementList[0]) AchivementOn(image1, 0);
+        if (mainData.otherData.money >= moneysList[1] || mainData.achivementData.moneysAchivementList[1]) AchivementOn(image2, 1);
+        if (mainData.otherData.money >= moneysList[2] || mainData.achivementData.moneysAchivementList[2]) AchivementOn(image3, 2);
+        if (mainData.otherData.money >= moneysList[3] || mainData.achivementData.moneysAchivementList[3]) AchivementOn(image4, 3);
+        if (mainData.otherData.money >= moneysList[4] || mainData.achivementData.moneysAchivementList[4]) AchivementOn(image5, 4);
+        if (mainData.otherData.money >= moneysList[5] || mainData.achivementData.moneysAchivementList[5]) AchivementOn(image6, 5);
+        if (mainData.otherData.money >= moneysList[6] || mainData.achivementData.moneysAchivementList[6]) AchivementOn(image7, 6);
+        if (mainData.otherData.money >= moneysList[7] || mainData.achivementData.moneysAchivementList[7]) AchivementOn(image8, 7);
+        if (mainData.otherData.money >= moneysList[8] || mainData.achivementData.moneysAchivementList[8]) AchivementOn(image9, 8);
+    }
 
-        //if (mainData.otherData.money <= moneysList[0])
-
-        /*for (int i = 0; i < 9; i++)
-        {
-            if (mainData.otherData.money <= moneysList[i] || mainData.achivementData.moneysAchivementList[i])
-            {
-                imagesMass[i] = checkerOn;
-            }
-        }*/
+    private void AchivementOn(Image image, int i)
+    {
+        image.overrideSprite = checkerOn;
+        mainData.achivementData.moneysAchivementList[i] = true;
     }
 
     public void Click(Button button)
     {
-        button.image = checkerOn;
+        button.image.sprite = checkerOn;
     }
 
     public void OpenClose()
