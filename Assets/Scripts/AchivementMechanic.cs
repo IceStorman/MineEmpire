e93ab69e43decc091f9ba3d601dcb85038038329
Jok55r <81425160+Jok55r@users.noreply.github.com>
2021-12-24 18:30:18 +0100

    Revert "adding achivements and there is a bug"
    This reverts commit 5fed0bdde62974a1eb6a09ee7d3b2dd55579881e.

--- Assets/Scripts/AchivementMechanic.cs
+++ Assets/Scripts/AchivementMechanic.cs
@@ -22,16 +22,6 @@ public class AchivementMechanic : MonoBehaviour
     [SerializeField] private Image image8;
     [SerializeField] private Image image9;
 
-    [SerializeField] private Image image2_1;
-    [SerializeField] private Image image2_2;
-    [SerializeField] private Image image2_3;
-    [SerializeField] private Image image2_4;
-    [SerializeField] private Image image2_5;
-    [SerializeField] private Image image2_6;
-    [SerializeField] private Image image2_7;
-    [SerializeField] private Image image2_8;
-    [SerializeField] private Image image2_9;
-
     private void Start()
     {
         panel.SetActive(false);
@@ -39,19 +29,7 @@ public class AchivementMechanic : MonoBehaviour
 
     private void Update()
     {
-        List<Image> images1 = new List<Image>(9) { image1, image2, image3, image4, image5, image6, image7, image8, image9 };
-        List<Image> images2 = new List<Image>(9) { image2_1, image2_2, image2_3, image2_4, image2_5, image2_6, image2_7, image2_8, image2_9 };
-
-        List<int> oresCount = new List<int>() { mainData.copperData.ore, mainData.tinData.ore, mainData.benitoiteData.ore, mainData.emeraldData.ore,
-            mainData.onyxData.ore, mainData.rubyData.ore, mainData.sapphireData.ore, mainData.diamondData.ore, mainData.amethystData.ore };
-
-        /*for (int i = 0; i < 8; i++)
-        { if (mainData.otherData.money >= moneysList[i] || mainData.achivementData.haveYouDone[i]) AchivementOn(images1[i], i); }*/
-
-        /*for (int i = 0; i < 8; i++)
-        { if (oresCount[i] > 0 || mainData.achivementData.haveYouDone2[i]) AchivementOn2(images2[i], i); }*/
-
-        /*if (mainData.otherData.money >= moneysList[0] || mainData.achivementData.moneysAchivementList[0]) AchivementOn(image1, 0);
+        if (mainData.otherData.money >= moneysList[0] || mainData.achivementData.moneysAchivementList[0]) AchivementOn(image1, 0);
         if (mainData.otherData.money >= moneysList[1] || mainData.achivementData.moneysAchivementList[1]) AchivementOn(image2, 1);
         if (mainData.otherData.money >= moneysList[2] || mainData.achivementData.moneysAchivementList[2]) AchivementOn(image3, 2);
         if (mainData.otherData.money >= moneysList[3] || mainData.achivementData.moneysAchivementList[3]) AchivementOn(image4, 3);
@@ -59,19 +37,13 @@ public class AchivementMechanic : MonoBehaviour
         if (mainData.otherData.money >= moneysList[5] || mainData.achivementData.moneysAchivementList[5]) AchivementOn(image6, 5);
         if (mainData.otherData.money >= moneysList[6] || mainData.achivementData.moneysAchivementList[6]) AchivementOn(image7, 6);
         if (mainData.otherData.money >= moneysList[7] || mainData.achivementData.moneysAchivementList[7]) AchivementOn(image8, 7);
-        if (mainData.otherData.money >= moneysList[8] || mainData.achivementData.moneysAchivementList[8]) AchivementOn(image9, 8);*/
+        if (mainData.otherData.money >= moneysList[8] || mainData.achivementData.moneysAchivementList[8]) AchivementOn(image9, 8);
     }
 
     private void AchivementOn(Image image, int i)
     {
         image.overrideSprite = checkerOn;
-        mainData.achivementData.haveYouDone[i] = true;
-    }
-
-    private void AchivementOn2(Image image, int i)
-    {
-        image.overrideSprite = checkerOn;
-        mainData.achivementData.haveYouDone2[i] = true;
+        mainData.achivementData.moneysAchivementList[i] = true;
     }
 
     public void OpenClose()
@@ -90,19 +62,10 @@ public class AchivementMechanic : MonoBehaviour
 
     public void Earn(int num)
     {
-        if (mainData.achivementData.haveYouDone[num] && !mainData.achivementData.haveYouEarned[num])
+        if (moneysList[num] <= mainData.otherData.money && !mainData.achivementData.haveYouEarned[num])
         {
             mainData.otherData.exp += 10 + (num * 5);
             mainData.achivementData.haveYouEarned[num] = true;
-        }
-    }
-
-    public void Earn2(int num)
-    {
-        if (mainData.achivementData.haveYouDone2[num] && !mainData.achivementData.haveYouEarned2[num])
-        {
-            mainData.otherData.exp += 10 + (num * 5);
-            mainData.achivementData.haveYouEarned2[num] = true;
         }
     }
 }
--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -45,8 +45,7 @@ public class Reset : MonoBehaviour
         ResetPlantData(mainData.allPlantsData.firstSpecialSectionData);
         ResetPlantData(mainData.allPlantsData.secondSpecialSectionData);
         ResetPlantData(mainData.allPlantsData.thirdSpecialSectionData);
-        ResetAchivementData(mainData.achivementData.haveYouDone, mainData.achivementData.haveYouEarned);
-        ResetAchivementData(mainData.achivementData.haveYouDone2, mainData.achivementData.haveYouEarned2);
+        ResetAchivementData(mainData.achivementData.moneysAchivementList, mainData.achivementData.haveYouEarned);
         Debug.LogWarning("Reseted");
     }
 
