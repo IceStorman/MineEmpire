ab9845c87276693b61964ff5aeb3963dffba4320
Jok55r <81425160+Jok55r@users.noreply.github.com>
2022-01-20 17:19:28 +0100

    Adddeed tutorial

---
+++ Assets/Scripts/OtherScripts/TutorialMechanic.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialMechanic : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Image tutorImage;
+    [SerializeField] private Text tutorText;
+
+    [SerializeField] private List<Sprite> pageImage = new List<Sprite>();
+    [SerializeField] private List<string> pageText = new List<string>();
+
+    bool isTutor = false;
+    int whatPageIsNow = 0;
+
+    private void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    public void StartedTutorial()
+    {
+        panel.SetActive(true);
+        NextPage();
+        isTutor = true;
+    }
+
+    private void EndTutorial()
+    {
+        panel.SetActive(false);
+        isTutor = false;
+    }
+
+    public void NextPage()
+    {
+        if (whatPageIsNow >= pageImage.Count)
+        {
+            whatPageIsNow = 0;
+            EndTutorial();
+            return;
+        }
+        tutorImage.sprite = pageImage[whatPageIsNow];
+        tutorText.text = pageText[whatPageIsNow];
+
+        whatPageIsNow++;
+    }
+}
